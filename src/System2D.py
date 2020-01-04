2b1207e1167010ed7c7dfd25a44ee8bf49cd6611
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-03 17:03:01 -0700

    toy 2D grid-and-solver in progress

--- src/System2D.py
+++ src/System2D.py
@@ -12,6 +12,7 @@ class Node(object):
         self.x0 = vector[0]
         self.x1 = vector[1]
         self.vector = vector
+        self.num_faces = len(self.vector)
         
         
 class Cell(object):
@@ -21,7 +22,10 @@ class Cell(object):
 
 class Grid(object):
     
-    def __init__(self, mesh=None, m=10,n=10):
+    def __init__(self, mesh=None, m=10,n=10,type_='rect'):
+        self.gridtype = {'rect':0,
+                         'tri':1}
+        self.type = type_
         if mesh is None:
             mesh = np.zeros((2,m,n),float) # C-ordering.  last index is most rapid
             self.dim = 2
@@ -46,8 +50,60 @@ class Grid(object):
                 self.nodes[i].append(Node(self.mesh[:,i,j]))
                 
         self.nodes = np.asarray(self.nodes)
+        
+        self.make_cells()
+        
+    def make_cells(self):
+        if self.type is 'rect':
+            self.make_rect_cells()
+        else:
+            self.make_tri_cells()
+        return
+    
+    def make_rect_cells(self):
+        
+        self.cells = []
+        for i in range(self.m-1):
+            self.cells.append([])
+            for j in  range(self.n-1):
+                self.cells[i].append(
+                                    Cell([
+                                            self.nodes[i,j],
+                                            self.nodes[i,j+1],
+                                            self.nodes[i+1,j],
+                                            self.nodes[i+1,j+1]
+                                        ])
+                )
                 
+        self.cells = np.asarray(self.cells)
+        return
+    
+    def make_tri_cells(self):
+        
+        self.cells = []
+        for i in range(self.m-1):
+            self.cells.append([])
+            for j in  range(self.n-1):
+                self.cells[i].append(
+                                    Cell([
+                                            self.nodes[i,j],
+                                            self.nodes[i,j+1],
+                                            self.nodes[i+1,j]
+                                        ])
+                )
+                self.cells[i].append(
+                                    Cell([
+                                            self.nodes[i,j+1],
+                                            self.nodes[i+1,j+1],
+                                            self.nodes[i+1,j]
+                                        ])
+                )
+                
+        self.cells = np.asarray(self.cells)
+        return
+        
         
         
 if __name__ == '__main__':
-    self = Grid()
+    gd = Grid(type_='rect')
+    self = Grid(type_='tri')
