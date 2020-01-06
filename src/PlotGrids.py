656499fa5a6934a6264de9c756491df9892da3b3
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-06 09:47:49 -0700

    missing plotting class

---
+++ src/PlotGrids.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 16:04:47 2020
+
+@author: lukemcculloch
+"""
+
+import numpy as np
+# imports for plotting:
+import matplotlib.pyplot as plt
+
+class PlotGrid(object):
+    
+    def __init__(self, grid):
+        self.grid = grid
+        
+    
+    def plot_cells(self, canvas = None,
+                   alpha=.1):
+        if canvas is None:
+            fig, ax = plt.subplots()
+            ax.axis('equal')
+        else:
+            ax = canvas
+        grid = self.grid
+        
+        #for cell in grid.cellList[:1]:
+        for cell in grid.cellList:
+            for face in cell.faces:
+                n0 = face.nodes[0]
+                n1 = face.nodes[1]
+                
+                ax.plot(n0.x0,n0.x1,
+                        color='red',
+                        marker='o',
+                        alpha = alpha)
+                ax.plot(n1.x0,n1.x1,
+                        color='red',
+                        marker='o',
+                        alpha = alpha)
+                
+                x = [n0.x0,n1.x0]
+                y = [n0.x1,n1.x1]
+                ax.plot(x, y,
+                        color='black',
+                        alpha = alpha)
+        
+        return ax
+    
+    
+    def plot_centroids(self, canvas = None,
+                       alpha=.1):
+        if canvas is None:
+            fig, ax = plt.subplots()
+        else:
+            ax = canvas
+        grid = self.grid
+        
+        
+        #for cell in grid.cellList[:1]:
+        for cell in grid.cellList:
+            ax.plot(cell.centroid[0],
+                    cell.centroid[1],
+                    color='green',
+                    marker='o',
+                    alpha = alpha,)
+        
+        return ax
+    
+    
+    def plot_face_centers(self, canvas = None,
+                       alpha=.1):
+        if canvas is None:
+            fig, ax = plt.subplots()
+        else:
+            ax = canvas
+        grid = self.grid
+        
+        
+        #for cell in grid.cellList[:1]:
+        for cell in grid.cellList:
+            for face in cell.faces:
+                
+                norm0 = face.normal_vector - face.center
+                norm1 = face.normal_vector - face.center
+                
+                ax.plot(face.center[0],
+                        face.center[1],
+                        color='yellow',
+                        marker='o',
+                        alpha = alpha)
+                
+                ax.plot(face.center[0],
+                        face.center[1],
+                        color='yellow',
+                        marker='o',
+                        alpha = alpha)
+        
+        return ax
+    
+    def plot_normals(self, canvas = None,
+                       alpha=.4):
+        if canvas is None:
+            fig, ax = plt.subplots()
+        else:
+            ax = canvas
+        grid = self.grid
+        
+        
+        #for cell in grid.cellList[:1]:
+        for cell in grid.cellList:
+            for face in cell.faces:
+                # print 'new face'
+                # print '\n Normal vector'
+                # print face.normal_vector
+                # print '\n center'
+                # print face.center
+                
+                norm0 = .5*face.normal_vector*face.area**2 + face.center
+                #norm0 = norm0*face.area
+                
+                #ax.plot([ norm0[0],face.center[0] ],
+                #        [ norm0[1],face.center[1] ],
+                #        color='purple',
+                #        marker='o',
+                #        alpha = alpha)
+                
+                #scalearrow = np.linalg.norm(norm0)
+                plt.arrow(face.center[0],
+                          face.center[1],
+                          norm0[0]-face.center[0] ,
+                          norm0[1]-face.center[1] )
+        return ax
+    
+    def plot_cell(self, cell, canvas = None,
+                  alpha=.4):
+        if canvas is None:
+            fig, ax = plt.subplots()
+        else:
+            ax = canvas
+        grid = self.grid
+        
+        
+        #for cell in grid.cellList[:1]:
+        for cell in grid.cellList:
+            for face in cell.faces:
+                
+                norm0 = face.normal_vector + face.center
+                
+                ax.plot([ norm0[0],face.center[0] ],
+                        [ norm0[1],face.center[1] ],
+                        color='black',
+                        marker='o',
+                        alpha = alpha)
+                
+        return ax
+                
