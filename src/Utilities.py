5264e46993def5b12d411be9df5a0aa638bd8ac3
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-12 15:34:08 -0700

    triangle area of cell or cell component

--- src/Utilities.py
+++ src/Utilities.py
@@ -149,12 +149,8 @@ def triangle_area(node1,node2,node3):
     q1 = node1.vector
     q2 = node2.vector
     q3 = node3.vector
-    x1 = q1[0]
-    x2 = q2[0]
-    x3 = q3[0]
-    y1 = q1[1]
-    y2 = q2[1]
-    y3 = q3[1]
+    x1,x2,x3 = q1[0],q2[0],q3[0]
+    y1,y2,y3 = q1[1],q2[1],q3[1]
     # area = 0.5*( (x1-x3)*(y2-y3)-(y1-y3)*(x2-x3) )      #<- cross product
     return  0.5*( x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2) ) #re-arranged
 
