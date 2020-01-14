5f09aef2996bef8f3b16fef4c1162b7f0a8ef720
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-13 21:39:12 -0700

    solution data and primative - conservative conversion

--- src/Solvers.py
+++ src/Solvers.py
@@ -14,18 +14,25 @@ def Solvers(object):
         self.mesh = mesh
         
         # solution data
-        self.u = np.zeros((mesh.nCells,nq),float)
-        self.w = np.zeros((mesh.nCells,nq),float)
-        self.u2w = np.zeros((mesh.nCells,nq),float)
+        self.u = np.zeros((mesh.nCells,nq),float) # conservative variables at cells/nodes
+        self.w = np.zeros((mesh.nCells,nq),float) # primative variables at cells/nodes
+        self.gradw = np.zeros((mesh.nCells,nq,2),float) # gradients of w at cells/nodes.
         # 
         # solution convergence
-        self.res = np.zeros((len(mesh.cellList),nq),float)
+        self.res = np.zeros((len(mesh.cellList),nq),float) #residual vector
         self.residual_norm = np.zeros((nq,1),float)
         
         # update step data
         self.u0 = np.zeros((mesh.nCells,nq),float)
         
+        # accessor integers for clarity
+        self.ir = 0 # density
+        self.iu = 1 # x-velocity
+        self.iv = 2 # y-velocity
+        self.ip = 3 # pressure
         
+        # fluid properties
+        self.gamma = 1.4 # Ratio of specific heats for air
         
         
         
@@ -69,6 +76,19 @@ def Solvers(object):
         return
     
     
+    
+    
+    #-------------------------------------------------------------------------#
+    #
+    # compute residuals
+    #
+    #-------------------------------------------------------------------------#
+    
+    #-------------------------------------------------------------------------#
+    #
+    # compute_residual: comptutes the local residual 
+    #
+    #-------------------------------------------------------------------------#
     def compute_residual_norm(self):
         return
     
@@ -83,5 +103,36 @@ def Solvers(object):
         for cell in mesh.cellList:
         return
     
+    
+    
+    #-------------------------------------------------------------------------#
+    #
+    # time stepping
+    #
+    #-------------------------------------------------------------------------#
     def compute_global_time_step(self):
+        return
+    
+    
+    #-------------------------------------------------------------------------#
+    #
+    # compute w from u
+    # ------------------------------------------------------------------------#
+    #  Input:  u = conservative variables (rho, rho*u, rho*v, rho*E)
+    # Output:  w =    primitive variables (rho,     u,     v,     p)
+    # ------------------------------------------------------------------------#
+    #
+    # Note:    E = p/(gamma-1)/rho + 0.5*(u^2+v^2)
+    #       -> p = (gamma-1)*rho*E-0.5*rho*(u^2+v^2)
+    # 
+    #
+    #-------------------------------------------------------------------------#
+    def u2w(self, u):
+        w = np.zeros((nq),float)
+        
+        w(self.ir) = u[0]
+        w(self.iu) = u[1]/u[0]
+        w(self.iv) = u[2]/u[0]
+        w(self.ip) = (self.gamma-1.0)*( u[3] - \
+                                       0.5*w[0]*(w[1]*w[1] + w[2]*w[2]) )
         return
