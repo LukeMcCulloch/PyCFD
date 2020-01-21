790b09ab14e8428f52edcddce584c5c187d6e91d
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-21 00:40:30 -0700

    BC (boundary condition) 'module' -- using python class as to be determined

---
+++ src/BoundaryConditions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 00:36:00 2020
+
+@author: lukemcculloch
+"""
+
+class BC_states(object):
+    
+    def __init__(self, p2):
+        self.p2 = p2
+    
+    
+    def get_right_state(self):
+        return
+    
+    
+    
+    def outflow_supersonic(self):
+        return
