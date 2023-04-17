b6d5ef968b7ea6d396aec0a0678a0fb9a8f40a11
Luke McCulloch <tlukemcculloch@gmail.com>
2023-04-16 19:49:12 -0600

    get paths may be useful

---
+++ src/getPaths.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 14:46:00 2023
+
+@author: lukemcculloch
+"""
+import sys, os
+lcldir = os.getcwd()
+sys.path.append(os.path.join(os.path.dirname(__file__), '../cases'))
+#sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'core'))
+#sys.path.append(os.path.join(lcldir, '..', '/cases'))
+
